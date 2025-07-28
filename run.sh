aa4288faaab09cedc7ecd860d375ee0e7b38207d
Thomas Barlow <barlowtj48@gmail.com>
2025-07-28 13:07:08 -0400

    Passthrough for datadump arguments

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Usage: ./run.sh dump|load
+# Usage: ./run.sh dump|load [additional sqldatadump args]
 # Requires environment variables:
 #   CONN - for both sqldatadump and sqldataload
 
@@ -9,15 +9,16 @@ set -e
 mkdir -p /data/table_data
 
 if [[ "$1" == "dump" ]]; then
+  shift
   sqldatadump --directory=/data/table_data \
               --schema=CORE \
               --rowsPerBatch=1000 \
               --batchesPerFile=10 \
               --ignoreTables=CarrierRef,MasterLocation,Instance \
-              "$CONN"
+              "$CONN" "$@"
 elif [[ "$1" == "load" ]]; then
   sqldataload --directory=/data/table_data "$CONN"
 else
-  echo "Usage: $0 dump|load"
+  echo "Usage: $0 dump|load [additional sqldatadump args]"
   exit 1
 fi
